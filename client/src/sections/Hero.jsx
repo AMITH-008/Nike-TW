230a17f195d250e59b7e0884bb8056fe3a555bb2
amith <amithkouti@gmail.com>
2024-01-01 23:16:05 +0530

    Implemented changing big shoe image based on user selection

--- client/src/sections/Hero.jsx
+++ client/src/sections/Hero.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from '../components/Button';
 import { arrowRight } from '../assets/icons';
-import { statistics } from '../constants';
+import { shoes, statistics } from '../constants';
 import { bigShoe1 } from '../assets/images';
+import ShoeCard from '../components/ShoeCard';
 
 const Hero = () => {
+  const [currentShoe, setCurrentShoe] = useState(shoes[0].bigShoe)
   return (
     <section 
       id='home'
@@ -45,12 +47,25 @@ const Hero = () => {
       </div>
       <div className='relative flex flex-1 justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center'>
         <img 
-          src={bigShoe1} 
+          src={currentShoe} 
           alt="shoe collection"
           width={610}
           height={500}
           className='relative z-10'
         />
+        <div className='flex sm:gap-6 gap-4 absolute -bottom-[8%] sm:left-[10%] max-sm:px-6'>
+          {
+            shoes.map(shoe => (
+              <div key={shoe}>
+                <ShoeCard 
+                imgURL={shoe}
+                changeBigShoeImage= {(newShoe) => {setCurrentShoe(newShoe)}}
+                bigShoeImg={currentShoe}
+                />
+              </div>
+            ))
+          }
+        </div>
       </div>       
     </section>
   )
